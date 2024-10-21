8721b6f14a3fe2a661c439bceb6e6b8259a964fb
Hugo <hugo.reyhol@educa.jcyl.es>
2024-10-21 12:31:41 +0200

    Creado metodo obtenerColeccion en CocheDAO

--- src/main/java/org/example/practicamongodb/dao/CocheDAO.java
+++ src/main/java/org/example/practicamongodb/dao/CocheDAO.java
@@ -6,17 +6,19 @@ import com.mongodb.client.MongoCursor;
 import org.bson.Document;
 import org.example.practicamongodb.model.Coche;
 import org.example.practicamongodb.util.ConnectionDB;
-
 import java.util.ArrayList;
-
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Sorts.descending;
 
+
 public class CocheDAO {
 
-    public static boolean guardarCoche(Coche coche) {
-        MongoCollection<Document> collection = ConnectionDB.getCon().getDatabase("practicaMongo").getCollection("coches");
+    private static MongoCollection<Document> obtenerColeccion() {
+        return ConnectionDB.getCon().getDatabase("practicaMongo").getCollection("coches");
+    }
 
+    public static boolean guardarCoche(Coche coche) {
+        MongoCollection<Document> collection = obtenerColeccion();
         Gson gson = new Gson();
 
         int cantidad;
@@ -44,8 +46,7 @@ public class CocheDAO {
     }
 
     public static boolean actualizarCoche(Coche cocheAntiguo, Coche cocheNuevo) {
-        MongoCollection<Document> collection = ConnectionDB.getCon().getDatabase("practicaMongo").getCollection("coches");
-
+        MongoCollection<Document> collection = obtenerColeccion();
         Gson gson = new Gson();
 
         Document filtro = new Document("matricula", new Document("$eq", cocheNuevo.getMatricula()))
@@ -72,8 +73,7 @@ public class CocheDAO {
     }
 
     public static void eliminarCoche(Coche coche) {
-        MongoCollection<Document> collection = ConnectionDB.getCon().getDatabase("practicaMongo").getCollection("coches");
-
+        MongoCollection<Document> collection = obtenerColeccion();
         Gson gson = new Gson();
 
         Document c = Document.parse(gson.toJson(coche));
@@ -85,8 +85,7 @@ public class CocheDAO {
     public static ArrayList<Coche> listarCoches() {
         ArrayList<Coche> coches = new ArrayList<>();
 
-        MongoCollection<Document> collection = ConnectionDB.getCon().getDatabase("practicaMongo").getCollection("coches");
-
+        MongoCollection<Document> collection = obtenerColeccion();
         Gson gson = new Gson();
 
         try (MongoCursor<Document> listaCoches = collection.find().iterator()) {
